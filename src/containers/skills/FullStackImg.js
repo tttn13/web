5611856fce86e80f8b9a4fc21500893e29a8382f
Thi Nguyen <tttn.tn13@gmail.com>
2025-03-18 20:49:36 +0700

    changed image

--- src/containers/skills/FullStackImg.js
+++ src/containers/skills/FullStackImg.js
@@ -4,7 +4,7 @@ export default class FullStackImg extends Component {
   render() {
     const theme = this.props.theme;
     return (
-      <img src="https://media0.giphy.com/media/v1.Y2lkPTc5MGI3NjExeDhkYjNuMGt1ZDR5NnFvYTEyY2UxMW1oZmduM2NhZTkzdzJ2YW5hdCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9cw/H1jSPXCJmo8AZi3gdP/giphy.gif" />
+      <img src="https://media4.giphy.com/media/v1.Y2lkPTc5MGI3NjExMXhtZWlzYzE1dXl5bmttanNweXMxN244YTExOXZqY3phbTFiZGEwMyZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9cw/yTFemEJxmeW2YLOT6p/giphy.gif" />
       // <img alt="full stack" src={require("../../assests/images/fullstack_theme.svg")}/>
     );
   }
